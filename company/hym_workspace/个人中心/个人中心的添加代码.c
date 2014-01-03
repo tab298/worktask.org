370ad938702364ab8fea36c3a24b64870531109a
luning git <luning298@gmail.com>
2014-01-03 11:28:45 +0800

    添加了有明的工作目录

---
+++ company/hym_workspace/个人中心/个人中心的添加代码.c
@@ -0,0 +1,10 @@
+//在个人中心的app里面添加如下代码的实现：
+extern "C" int Gui_Init_UserBill(){}//在系统启动时，进行的初始化。
+
+extern "C" int Gui_Uninit_UserBill(){}//
+
+extern "C" int Gui_GetInf_UserBill(int MSG,  void* wParam,void* lParam){}//提供给其他模块调用获取个人中心里面的信息
+
+extern "C" message_callback_t Gui_MsgCallBack_UserBill(Gui_Message MSG,  int wParam,int lParam){} //鼠标点击，或者按键信息的回调函数
+
+extern "C" int Gui_Main_USERBILL(int argc, char *argv[]);//主函数的入口。
