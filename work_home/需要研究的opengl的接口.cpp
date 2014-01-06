3267427186ed53530ce880f76dc389c30a36e663
luning git <luning298@gmail.com>
2014-01-06 17:48:27 +0800

    添加了opengl的接口

---
+++ work_home/需要研究的opengl的接口.cpp
@@ -0,0 +1,68 @@
+ glCreateProgram
+ glAttachShader
+ glBindAttribLocation
+ glLinkProgram
+ glGetProgramiv
+ glUseProgram
+ 
+ glGetProgramInfoLog
+ 
+ 
+ 
+ glCreateShader
+ glShaderSource
+ glCompileShader
+ glGetShaderiv
+ glGetShaderInfoLog
+ glDeleteShader
+ 
+ glDeleteProgram
+ 
+ glGetUniformLocation
+ glUniform1i
+ 
+ glDeleteBuffers
+ 
+ 
+ glGenBuffers
+ glBindBuffer
+ glBufferData
+ 
+ 
+ glViewport
+ 
+ MGOGLESDestroy
+ MGAppExit
+ 
+ 
+ MGOGLESRender
+ 
+ MGAppInit
+ MGAppGetData
+ MGOGLESCreate
+ 
+ 
+ 
+ 
+ glUniform2f
+ glUniformMatrix2fv
+ glEnableVertexAttribArray
+ glVertexAttribPointer
+ glDrawArrays
+ glDisableVertexAttribArra
+ 
+ 
+ glClear
+ glDisable
+ glEnable
+ 
+ glDepthFunc
+ glClearDepthf
+ glClearColor
+ glBlendFunc
+ 
+ glTexSubImage2D
+ 
+ 
+ 
+ 
